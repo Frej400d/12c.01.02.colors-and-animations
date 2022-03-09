240ba1307b5ad582beaea80c0fa8cfdc8e3bcd44
Freja <frej400d@stud.kea.dk>
2022-03-09 01:39:58 +0100

    night mode done ?

---
+++ dark_mode.js
@@ -0,0 +1,32 @@
+// Change theme to dark by adding the `dark` classname to html element.
+let changeThemeToDark = () => {
+  document.documentElement.setAttribute("data-theme", "dark"); //set theme to light
+};
+
+// Reset the html class to default
+changeThemeToDark = () => {
+  document.documentElement.setAttribute("data-theme", "light"); //set theme to light
+};
+
+let theme = localStorage.getItem("data-theme");
+changeThemeToDark = () => {
+  document.documentElement.setAttribute("data-theme", "dark"); // set theme to dark
+  localStorage.setItem("data-theme", "dark"); // save theme to local storage
+};
+
+changeThemeToLight = () => {
+  document.documentElement.setAttribute("data-theme", "light"); // set theme light
+  localStorage.setItem("data-theme", "light"); // save theme to local storage
+};
+
+// Get the element based on ID
+const checkbox = document.getElementById("switch");
+// Apply retrived them to the website
+checkbox.addEventListener("change", () => {
+  let theme = localStorage.getItem("data-theme"); // Retrieve saved them from local storage
+  if (theme === "dark") {
+    changeThemeToLight();
+  } else {
+    changeThemeToDark();
+  }
+});
